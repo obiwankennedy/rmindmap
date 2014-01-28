9a49980d7df3e58ab988d5d88bad678fde15a821
obiwankennedy <renaud@rolisteam.org>
2014-01-28 15:38:06 +0100

    Update detailpanel.cpp
    make it more generic

--- src/detailpanel/detailpanel.cpp
+++ src/detailpanel/detailpanel.cpp
@@ -7,25 +7,21 @@ DetailPanel::DetailPanel(QWidget *parent) :
 {
     m_currentEdge = NULL;
     m_currentNode = NULL;
+    m_currentPackage = NULL;
     ui->setupUi(this);
 
+    /// Generic properties
+    connect(ui->m_textEdit,SIGNAL(textEdited(QString)),this,SLOT(updateGenericItem()));
+    connect(ui->m_xEdit,SIGNAL(valueChanged(int)),this,SLOT(updateGenericItem()));
+    connect(ui->m_yEdit,SIGNAL(valueChanged(int)),this,SLOT(updateGenericItem()));
+    connect(ui->m_heightEdit,SIGNAL(valueChanged(int)),this,SLOT(updateGenericItem()));
+    connect(ui->m_widthEdit,SIGNAL(valueChanged(int)),this,SLOT(updateGenericItem()));
+
     connect(ui->m_nodeDescriptionEdit,SIGNAL(textChanged()),this,SLOT(updateCurrentNode()));
-    connect(ui->m_nodeTextEdit,SIGNAL(textEdited(QString)),this,SLOT(updateCurrentNode()));
     connect(ui->m_bgColorChooser,SIGNAL(colorChanged(QColor)),this,SLOT(updateCurrentNode()));
     connect(ui->m_borderColorChooser,SIGNAL(colorChanged(QColor)),this,SLOT(updateCurrentNode()));
-
-
-    connect(ui->m_edgeTextEdit,SIGNAL(textEdited(QString)),this,SLOT(updateCurrentEdge()));
     connect(ui->m_edgeArrowType,SIGNAL(currentIndexChanged(int)),this,SLOT(updateCurrentEdge()));
-
-
-    connect(ui->m_packageText,SIGNAL(textEdited(QString)),this,SLOT(updateCurrentPackage()));
     connect(ui->m_packageLine,SIGNAL(currentIndexChanged(int)),this,SLOT(updateCurrentPackage()));
-
-    connect(ui->m_xEdit,SIGNAL(valueChanged(int)),this,SLOT(updateCurrentPackage()));
-    connect(ui->m_yEdit,SIGNAL(valueChanged(int)),this,SLOT(updateCurrentPackage()));
-
-
 }
 
 DetailPanel::~DetailPanel()
@@ -40,7 +36,7 @@ void DetailPanel::setCurrentNode(Node* node)
     m_currentPackage = NULL;
     ui->m_stackedWidget->setCurrentIndex(0);
     ui->m_nodeDescriptionEdit->setText(node->description());
-    ui->m_nodeTextEdit->setText(node->getText());
+    ui->m_textEdit->setText(node->getText());
     ui->m_bgColorChooser->setColor(node->bgColor());
     ui->m_borderColorChooser->setColor(node->color());
 
@@ -54,7 +50,7 @@ void DetailPanel::setCurrentEdge(Edge* edge)
     m_currentNode = NULL;
     m_currentPackage = NULL;
     ui->m_stackedWidget->setCurrentIndex(1);
-    ui->m_edgeTextEdit->setText(edge->getText());
+    ui->m_textEdit->setText(edge->getText());
     ui->m_edgeArrowType->setCurrentIndex((int)edge->getKind());
     m_currentEdge = edge;
 }
@@ -66,17 +62,16 @@ void DetailPanel::updateCurrentNode()
     if(NULL!=m_currentNode)
     {
         m_currentNode->setDescription(ui->m_nodeDescriptionEdit->document()->toHtml());
-        m_currentNode->setText(ui->m_nodeTextEdit->text());
         m_currentNode->setColor(ui->m_borderColorChooser->color());
         m_currentNode->setBgColor(ui->m_bgColorChooser->color());
     }
+
 }
 
 void DetailPanel::updateCurrentEdge()
 {
     if(NULL!=m_currentEdge)
     {
-        m_currentEdge->setText(ui->m_edgeTextEdit->text());
         m_currentEdge->setKind((Edge::EndKind)ui->m_edgeArrowType->currentIndex());
     }
 }
@@ -85,9 +80,17 @@ void DetailPanel::updateCurrentPackage()
 {
     if(NULL!=m_currentPackage)
     {
-        m_currentPackage->setText(ui->m_packageText->text());
         m_currentPackage->setBorder((PackageItem::Border)ui->m_packageLine->currentIndex());
         m_currentPackage->setPos(ui->m_xEdit->value(),ui->m_yEdit->value());
+    }
+}
+void DetailPanel::updateGenericItem()
+{
+    if(NULL!=m_currentGenericItem)
+    {
+        m_currentGenericItem->setText(ui->m_textEdit->text());
+        m_currentGenericItem->setPos(ui->m_xEdit->value(),ui->m_yEdit->value());
+        m_currentGenericItem->setGeometry(ui->m_widthEdit->value(),ui->m_heightEdit->value());
     }
 }
 void DetailPanel::setCurrentPackage(PackageItem* p)
@@ -96,25 +99,46 @@ void DetailPanel::setCurrentPackage(PackageItem* p)
     m_currentNode = NULL;
     m_currentPackage = NULL;
     ui->m_stackedWidget->setCurrentIndex(2);
-    ui->m_packageText->setText(p->getText());
-    ui->m_xEdit->setValue(p->pos().x());
-    ui->m_yEdit->setValue(p->pos().y());
+    ui->m_textEdit->setText(p->getText());
+
 
     m_currentPackage = p;
 }
 
 void DetailPanel::setCurrentGenericItem(GenericMindMapItem* p)
 {
+    m_currentEdge = NULL;
+    m_currentNode = NULL;
+    m_currentPackage = NULL;
+    m_currentGenericItem = NULL;
+    ui->m_xEdit->setValue(p->pos().x());
+    ui->m_yEdit->setValue(p->pos().y());
+    ui->m_widthEdit->setValue(p->boundingRect().width());
+    ui->m_heightEdit->setValue(p->boundingRect().height());
+    m_currentGenericItem = p;
     if(NULL!=dynamic_cast<PackageItem*>(p))
     {
+        ui->m_xEdit->setEnabled(true);
+        ui->m_yEdit->setEnabled(true);
+        ui->m_widthEdit->setEnabled(true);
+        ui->m_heightEdit->setEnabled(true);
         setCurrentPackage(dynamic_cast<PackageItem*>(p));
     }
     else if(NULL!=dynamic_cast<Node*>(p))
     {
+        ui->m_xEdit->setEnabled(true);
+        ui->m_yEdit->setEnabled(true);
+        ui->m_widthEdit->setEnabled(false);
+        ui->m_heightEdit->setEnabled(false);
         setCurrentNode(dynamic_cast<Node*>(p));
     }
     else if(NULL!=dynamic_cast<Edge*>(p))
     {
+        ui->m_xEdit->setEnabled(false);
+        ui->m_yEdit->setEnabled(false);
+        ui->m_widthEdit->setEnabled(false);
+        ui->m_heightEdit->setEnabled(false);
         setCurrentEdge(dynamic_cast<Edge*>(p));
     }
+
 }
