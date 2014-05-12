2259bf23a6a04b2238c05a6c96d8106cd0c068d0
Renaud G. <renaud@rolisteam.org>
2014-05-12 09:25:54 +0200

    Update edge.cpp
    removal old comment header and fix edge on 4 points on node for the source.

--- src/edge.cpp
+++ src/edge.cpp
@@ -1,43 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
-**     the names of its contributors may be used to endorse or promote
-**     products derived from this software without specific prior written
-**     permission.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #include <QPainter>
 #include <QGraphicsProxyWidget>
 
@@ -139,6 +99,33 @@ void Edge::adjust()
     if (length > qreal(20.))
     {
         QPointF edgeOffset((line.dx() * (m_source->boundingRect().width()/2)) / length, (line.dy() * (m_source->boundingRect().height()/2)) / length);
+
+        if(abs(line.dx())<10)
+        {
+            edgeOffset.setX(0);
+            if(line.dy()>0)
+            {
+                edgeOffset.setY(m_source->boundingRect().height()/2);
+            }
+            else
+            {
+                edgeOffset.setY(-m_source->boundingRect().height()/2);
+            }
+        }
+        else
+        {
+            edgeOffset.setY(0);
+            if(line.dx()>0)
+            {
+                edgeOffset.setX(m_source->boundingRect().width()/2);
+            }
+            else
+            {
+                edgeOffset.setX(-m_source->boundingRect().width()/2);
+            }
+        }
+
+
         sourcePoint = line.p1() + edgeOffset;
         if(NULL!=m_dest)
         {
@@ -155,6 +142,9 @@ void Edge::adjust()
             destPoint = line.p1();
         }
     }
+
+
+
 
 
 }
