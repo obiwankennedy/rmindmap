fa6f8218a67bf342d698d36ceef6aa069082dbc0
Renaud G. <renaud@rolisteam.org>
2014-05-14 18:27:45 +0200

    Create timelinewidget.h

---
+++ src/widgets/timelinewidget.h
@@ -0,0 +1,15 @@
+#ifndef TIMELINEWIDGET_H
+#define TIMELINEWIDGET_H
+
+#include <QWidget>
+
+class TimeLineWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    TimeLineWidget();
+
+
+};
+
+#endif // TIMELINEWIDGET_H
