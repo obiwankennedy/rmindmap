ae85d8e1eb81f26c13ba62abfc73d8523d880755
Renaud G. <renaud@rolisteam.org>
2014-05-19 19:04:20 +0200

    Update timelinewidget.cpp
    adding drawing method

--- src/widgets/timelinewidget.cpp
+++ src/widgets/timelinewidget.cpp
@@ -17,8 +17,48 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+#include <QPainter>
+#include <QRect>
+#include <QSize>
+#include <QPalette>
+
 #include "timelinewidget.h"
 
 TimeLineWidget::TimeLineWidget()
+    : m_divisionCount(100),m_currentPosition(0)
 {
+    setMinimumSize(200,20);
+}
+void TimeLineWidget::paintEvent( QPaintEvent * event)
+{
+    QPainter painter(this);
+
+    painter.setBrush(palette().color(QPalette::AlternateBase));
+    painter.drawRoundedRect(0,0,size().width(),size().height(),10.0,10.0);
+    painter.setBrush(Qt::NoBrush);
+    //painter.fillRect(0,0,size().width(),size().height(),);
+
+    int step=(size().width()-10)/m_divisionCount;
+    int height=geometry().height();
+    //int widget=rect().width();
+    for(quint32 i = 0;i<m_divisionCount;++i)
+    {
+        painter.drawLine(10+i*step,height,10+i*step,height/2);
+    }
+
+
+    if(m_currentPosition>m_totalTime)
+    {
+        m_currentPosition=m_totalTime;
+    }
+    qreal pos = m_currentPosition*(size().width()-10)/m_totalTime;
+
+    painter.setPen(Qt::red);
+    painter.drawLine(pos+10,height,pos+10,height/2);
+
+}
+void TimeLineWidget::setIntervalTime(qreal a)
+{
+    m_intervalTime = a;
+    m_totalTime = m_intervalTime*m_divisionCount;
 }
